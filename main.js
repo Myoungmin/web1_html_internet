6137fa6b0367d9d1dd9dcf1fe1c33941c8170af4
Myoungmin <miung222@gmail.com>
2022-06-23 17:13:08 +0900

    Node.js26.App 제작-함수를 이용해서 정리 정돈하기
    templateHTML, templateList 함수 정의하여 코드 중복 제거

--- main.js
+++ main.js
@@ -2,68 +2,53 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title, list, body) {
+  return `
+  <!doctype html>
+<html>
+<head>
+<title>WEB1 - ${title}</title>
+<meta charset="utf-8">
+</head>
+<body>
+<h1><a href="/">WEB</a></h1>
+${list}
+${body}
+</body>
+</html>
+  `;
+}
+
+function templateList(filelist) {
+  var list = '<ul>';   
+  for (i in filelist) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+  }
+  list = list + '</ul>';
+  return list;
+}
+
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
   if (pathname === '/') {
     if (queryData.id === undefined) {
-
       fs.readdir('./data', function (error, filelist) {
         var title = 'Welcome';
         var description = 'Hello, Node.js';
-        var list = '<ul>';
-        //파일 디렉토리에서 읽어와 목록 생성     
-        for (i in filelist) {
-          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-        }
-        list = list + '</ul>';
-        var template = `
-        <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      ${list}
-      <h2>${title}</h2>
-      <p>${description}</p>
-    </body>
-    </html>
-        `;
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
         response.writeHead(200);
         response.end(template);
       })
     }
     else {
       fs.readdir('./data', function (error, filelist) {
-        var title = 'Welcome';
-        var description = 'Hello, Node.js';
-        var list = '<ul>';
-        //파일 디렉토리에서 읽어와 목록 생성     
-        for (i in filelist) {
-          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-        }
-        list = list + '</ul>';
         fs.readFile(`data/${queryData.id}`, `utf-8`, function (err, description) {
           var title = queryData.id;
-          var template = `
-        <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      ${list}
-      <h2>${title}</h2>
-      <p>${description}</p>
-    </body>
-    </html>
-        `;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
           response.writeHead(200);
           response.end(template);
         });
